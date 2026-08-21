159a0cdb178f4b4e7f8e4b658134a0ecb10f7739
johan5994 <miniora.construction@gmail.com>
2026-08-21 03:17:34 +0200

    Delete netlify/functions/get-config.js

--- netlify/functions/get-config.js
+++
@@ -1,5 +0,0 @@
-exports.handler = async () => ({
-  statusCode: 200,
-  headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
-  body: JSON.stringify({ docuseal_key: process.env.DOCUSEAL_API_KEY || null })
-});
